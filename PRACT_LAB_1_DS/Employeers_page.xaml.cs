f36e1af3f875784e45272657ab98858c2df0d00b
pvk <sergpilk33@gmail.com>
2024-03-19 10:19:13 +0300

    v1

---
+++ PRACT_LAB_1_DS/Employeers_page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PRACT_LAB_1_DS.VOD_DSTableAdapters;
+
+namespace PRACT_LAB_1_DS
+{
+    /// <summary>
+    /// Логика взаимодействия для Employeers_page.xaml
+    /// </summary>
+    public partial class Employeers_page : Page
+    {
+        CummonEmplTableAdapter cummon = new CummonEmplTableAdapter();
+        public Employeers_page()
+        {
+            InitializeComponent();
+            Table.ItemsSource = cummon.GetData();
+        }
+
+/*        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Table.Columns[4].Visibility = Visibility.Collapsed;
+        }*/
+    }
+}
---
+++ PRACT_LAB_1_DS/VOD_DS.cs
@@ -0,0 +1,15 @@
+﻿namespace PRACT_LAB_1_DS
+{
+
+
+    partial class VOD_DS
+    {
+    }
+}
+
+namespace PRACT_LAB_1_DS.VOD_DSTableAdapters {
+    
+    
+    public partial class EmployeersTableAdapter {
+    }
+}
